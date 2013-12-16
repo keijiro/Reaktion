37164c9a548f614beb4d1f1c5521b87b6a338cf6
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-12-16 14:31:46 +0900

    Added ReaktorToMessage.

---
+++ Assets/Reaktion/Editor/ReaktorToMessageEditor.cs
@@ -0,0 +1,107 @@
+﻿//
+// Reaktion - An audio reactive animation toolkit for Unity.
+//
+// Copyright (C) 2013 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ReaktorToMessage)), CanEditMultipleObjects]
+public class ReaktorToMessageEditor : Editor
+{
+    #region References to the properties
+
+    // Target settings.
+    SerializedProperty propTarget;
+    SerializedProperty propBroadcast;
+
+    // Input message.
+    SerializedProperty propEnableInput;
+    SerializedProperty propInputCurve;
+    SerializedProperty propMaxInput;
+    SerializedProperty propInputMessage;
+
+    // Trigger message.
+    SerializedProperty propEnableTrigger;
+    SerializedProperty propTriggerThreshold;
+    SerializedProperty propTriggerMessage;
+
+    #endregion
+
+    #region Editor functions
+
+    void OnEnable ()
+    {
+        // Target settings.
+        propTarget = serializedObject.FindProperty ("target");
+        propBroadcast = serializedObject.FindProperty ("broadcast");
+
+        // Input message.
+        propEnableInput = serializedObject.FindProperty ("enableInput");
+        propInputCurve = serializedObject.FindProperty ("inputCurve");
+        propInputMessage = serializedObject.FindProperty ("inputMessage");
+
+        // Trigger message.
+        propEnableTrigger = serializedObject.FindProperty ("enableTrigger");
+        propTriggerThreshold = serializedObject.FindProperty ("triggerThreshold");
+        propTriggerMessage = serializedObject.FindProperty ("triggerMessage");
+    }
+
+    public override void OnInspectorGUI ()
+    {
+        // Update the references.
+        serializedObject.Update ();
+
+        // Target settings.
+        propTarget.objectReferenceValue = EditorGUILayout.ObjectField ("Send To", propTarget.objectReferenceValue, typeof(GameObject), true);
+        if (propTarget.objectReferenceValue == null)
+        {
+            EditorGUILayout.HelpBox ("Leave None to send messages to itself (loopback)", MessageType.None);
+        }
+        else
+        {
+            propBroadcast.boolValue = EditorGUILayout.Toggle ("Broadcast", propBroadcast.boolValue);
+        }
+        EditorGUILayout.Space ();
+
+        // Input message.
+        propEnableInput.boolValue = EditorGUILayout.Toggle ("Input Message", propEnableInput.boolValue);
+        if (propEnableInput.boolValue)
+        {
+            propInputCurve.animationCurveValue = EditorGUILayout.CurveField ("Input Curve", propInputCurve.animationCurveValue);
+            propInputMessage.stringValue = EditorGUILayout.TextField ("Message", propInputMessage.stringValue);
+            EditorGUILayout.Space ();
+        }
+
+        // Trigger message.
+        propEnableTrigger.boolValue = EditorGUILayout.Toggle ("Trigger Message", propEnableTrigger.boolValue);
+        if (propEnableTrigger.boolValue)
+        {
+            EditorGUILayout.Slider (propTriggerThreshold, 0.0f, 1.0f, "Threahold");
+            propTriggerMessage.stringValue = EditorGUILayout.TextField ("Message", propTriggerMessage.stringValue);
+        }
+
+        // Apply modifications.
+        serializedObject.ApplyModifiedProperties ();
+    }
+
+    #endregion
+}
---
+++ Assets/Test/MessageReceiver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageReceiver : MonoBehaviour
+{
+    void OnReaktorInput (float level)
+    {
+        Debug.Log (level);
+    }
+
+    void OnReaktorTrigger ()
+    {
+        Debug.Log ("Bang!");
+    }
+}
