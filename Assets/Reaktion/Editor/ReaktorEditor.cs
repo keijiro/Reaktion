7d871aca49daa9cadde2d3ea9ba4dfec8acbac23
Keijiro Takahashi <keijiro@gmail.com>
2014-05-25 17:49:37 +0900

    Multiple object editing support.

--- Assets/Reaktion/Editor/ReaktorEditor.cs
+++ Assets/Reaktion/Editor/ReaktorEditor.cs
@@ -24,11 +24,9 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
-[CustomEditor(typeof(Reaktor))]
+[CustomEditor(typeof(Reaktor)), CanEditMultipleObjects]
 public class ReaktorEditor : Editor
 {
-    #region References to the properties
-
     // Audio input settings.
     SerializedProperty propAudioMode;
     SerializedProperty propAudioIndex;
@@ -57,259 +55,296 @@ public class ReaktorEditor : Editor
     SerializedProperty propLowerBound;
     SerializedProperty propFalldown;
 
-    #endregion
-
-    #region Private variables
+    // String labels.
+    GUIContent labelAudioSource;
+    GUIContent labelGainControl;
+    GUIContent labelOffsetControl;
+    GUIContent labelIndex;
+    GUIContent labelChannel;
+    GUIContent labelBandIndex;
+    GUIContent labelCurve;
+    GUIContent labelMidiCC;
+    GUIContent labelMidiChannel;
 
     // Texutres for drawing level bars.
     Texture2D[] barTextures;
 
-    #endregion
-
-    #region UI constants
-
-    static string[] sourceLabels = {
-        "No Input",
-        "RMS Level (mono)",
-        "RMS Level (L+R)",
-        "Frequency Band"
+    // UI contents.
+    static GUIContent[] sourceLabels = {
+        new GUIContent("No Input"),
+        new GUIContent("RMS Level (mono)"),
+        new GUIContent("RMS Level (L+R)"),
+        new GUIContent("Frequency Band")
     };
     static int[] sourceOptions = {
         0, 1, 2, 3
     };
-    static string[] midiChannelLabels = {
-        "Channel 1",
-        "Channel 2",
-        "Channel 3",
-        "Channel 4",
-        "Channel 5",
-        "Channel 6",
-        "Channel 7",
-        "Channel 8",
-        "Channel 9",
-        "Channel 10",
-        "Channel 11",
-        "Channel 12",
-        "Channel 13",
-        "Channel 14",
-        "Channel 15",
-        "Channel 16",
-        "All Channels"
+    static GUIContent[] midiChannelLabels = {
+        new GUIContent("Channel 1"),
+        new GUIContent("Channel 2"),
+        new GUIContent("Channel 3"),
+        new GUIContent("Channel 4"),
+        new GUIContent("Channel 5"),
+        new GUIContent("Channel 6"),
+        new GUIContent("Channel 7"),
+        new GUIContent("Channel 8"),
+        new GUIContent("Channel 9"),
+        new GUIContent("Channel 10"),
+        new GUIContent("Channel 11"),
+        new GUIContent("Channel 12"),
+        new GUIContent("Channel 13"),
+        new GUIContent("Channel 14"),
+        new GUIContent("Channel 15"),
+        new GUIContent("Channel 16"),
+        new GUIContent("All Channels")
     };
     static int[] midiChannelOptions = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
     };
-    
-    #endregion
-
-    #region Editor functions
 
     // On Enable (initialization)
-    void OnEnable ()
+    void OnEnable()
     {
         // Audio input settings.
-        propAudioMode = serializedObject.FindProperty ("audioMode");
-        propAudioIndex = serializedObject.FindProperty ("audioIndex");
-        propAudioCurve = serializedObject.FindProperty ("audioCurve");
+        propAudioMode         = serializedObject.FindProperty("audioMode");
+        propAudioIndex        = serializedObject.FindProperty("audioIndex");
+        propAudioCurve        = serializedObject.FindProperty("audioCurve");
 
         // Gain control.
-        propGainEnabled = serializedObject.FindProperty ("gainEnabled");
-        propGainKnobIndex = serializedObject.FindProperty ("gainKnobIndex");
-        propGainKnobChannel = serializedObject.FindProperty ("gainKnobChannel");
-        propGainCurve = serializedObject.FindProperty ("gainCurve");
+        propGainEnabled       = serializedObject.FindProperty("gainEnabled");
+        propGainKnobIndex     = serializedObject.FindProperty("gainKnobIndex");
+        propGainKnobChannel   = serializedObject.FindProperty("gainKnobChannel");
+        propGainCurve         = serializedObject.FindProperty("gainCurve");
 
         // Offset control.
-        propOffsetEnabled = serializedObject.FindProperty ("offsetEnabled");
-        propOffsetKnobIndex = serializedObject.FindProperty ("offsetKnobIndex");
-        propOffsetKnobChannel = serializedObject.FindProperty ("offsetKnobChannel");
-        propOffsetCurve = serializedObject.FindProperty ("offsetCurve");
+        propOffsetEnabled     = serializedObject.FindProperty("offsetEnabled");
+        propOffsetKnobIndex   = serializedObject.FindProperty("offsetKnobIndex");
+        propOffsetKnobChannel = serializedObject.FindProperty("offsetKnobChannel");
+        propOffsetCurve       = serializedObject.FindProperty("offsetCurve");
 
         // General option.
-        propSensitivity = serializedObject.FindProperty ("sensitivity");
-        propDecaySpeed = serializedObject.FindProperty ("decaySpeed");
+        propSensitivity       = serializedObject.FindProperty("sensitivity");
+        propDecaySpeed        = serializedObject.FindProperty("decaySpeed");
         
         // Audio input options.
-        propShowAudioOptions = serializedObject.FindProperty ("showAudioOptions");
-        propHeadroom = serializedObject.FindProperty ("headroom");
-        propDynamicRange = serializedObject.FindProperty ("dynamicRange");
-        propLowerBound = serializedObject.FindProperty ("lowerBound");
-        propFalldown = serializedObject.FindProperty ("falldown");
+        propShowAudioOptions  = serializedObject.FindProperty("showAudioOptions");
+        propHeadroom          = serializedObject.FindProperty("headroom");
+        propDynamicRange      = serializedObject.FindProperty("dynamicRange");
+        propLowerBound        = serializedObject.FindProperty("lowerBound");
+        propFalldown          = serializedObject.FindProperty("falldown");
+
+        // String labels.
+        labelAudioSource   = new GUIContent("Audio Source");
+        labelGainControl   = new GUIContent("Gain Control");
+        labelOffsetControl = new GUIContent("Offset Control");
+        labelIndex         = new GUIContent("Index");
+        labelChannel       = new GUIContent("Channel");
+        labelBandIndex     = new GUIContent("Band Index");
+        labelCurve         = new GUIContent("Curve");
+        labelMidiCC        = new GUIContent("MIDI CC#");
+        labelMidiChannel   = new GUIContent("MIDI Channel");
     }
     
     // On Disable (cleanup)
-    void OnDisable ()
+    void OnDisable()
     {
         if (barTextures != null)
         {
             // Destroy the bar textures.
             foreach (var texture in barTextures)
-                DestroyImmediate (texture);
+                DestroyImmediate(texture);
             barTextures = null;
         }
     }
 
+    GUIContent GetAudioIndexLabel()
+    {
+        if (propAudioMode.hasMultipleDifferentValues)
+        {
+            return labelIndex;
+        }
+        else if (propAudioMode.intValue == (int)Reaktor.AudioMode.SpecturmBand)
+        {
+            return labelBandIndex;
+        }
+        else
+        {
+            return labelChannel;
+        }
+    }
+
     // Shows the inspector.
-    public override void OnInspectorGUI ()
+    public override void OnInspectorGUI()
     {
         // Update the references.
-        serializedObject.Update ();
+        serializedObject.Update();
 
         // Audio input settings.
-        propAudioMode.intValue = EditorGUILayout.IntPopup ("Audio Source", propAudioMode.intValue, sourceLabels, sourceOptions);
-        if (propAudioMode.intValue > 0)
+        EditorGUILayout.IntPopup(propAudioMode, sourceLabels, sourceOptions, labelAudioSource);
+        if (propAudioMode.hasMultipleDifferentValues || propAudioMode.intValue > 0)
         {
-            var label = (propAudioMode.intValue == (int)Reaktor.AudioMode.SpecturmBand) ? "Band Index" : "Channel";
-            propAudioIndex.intValue = EditorGUILayout.IntField (label, propAudioIndex.intValue);
-            propAudioCurve.animationCurveValue = EditorGUILayout.CurveField ("Curve", propAudioCurve.animationCurveValue);
+            EditorGUILayout.PropertyField(propAudioIndex, GetAudioIndexLabel());
+            EditorGUILayout.PropertyField(propAudioCurve, labelCurve);
         }
 
-        // Gain control.
-        if (propAudioMode.intValue > 0)
-        {
-            if (propGainEnabled.boolValue || propOffsetEnabled.boolValue)
-                EditorGUILayout.Space ();
+        var shouldInsertSpace = propGainEnabled.hasMultipleDifferentValues || propGainEnabled.boolValue ||
+                                propOffsetEnabled.hasMultipleDifferentValues || propOffsetEnabled.boolValue;
 
-            if (propGainEnabled.boolValue = EditorGUILayout.Toggle ("Gain Control", propGainEnabled.boolValue))
+        // Gain control.
+        if (propAudioMode.hasMultipleDifferentValues || propAudioMode.intValue > 0)
+        {
+            if (shouldInsertSpace) EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(propGainEnabled, labelGainControl);
+            if (propGainEnabled.hasMultipleDifferentValues || propGainEnabled.boolValue)
             {
-                propGainKnobIndex.intValue = EditorGUILayout.IntField ("MIDI CC #", propGainKnobIndex.intValue);
-                propGainKnobChannel.intValue = EditorGUILayout.IntPopup ("MIDI Channel", propGainKnobChannel.intValue, midiChannelLabels, midiChannelOptions);
-                propGainCurve.animationCurveValue = EditorGUILayout.CurveField ("Curve", propGainCurve.animationCurveValue);
+                EditorGUILayout.PropertyField(propGainKnobIndex, labelMidiCC);
+                EditorGUILayout.IntPopup(propGainKnobChannel, midiChannelLabels, midiChannelOptions, labelMidiChannel);
+                EditorGUILayout.PropertyField(propGainCurve, labelCurve);
             }
         }
 
         // Offset control.
-        if (propGainEnabled.boolValue || propOffsetEnabled.boolValue)
-            EditorGUILayout.Space ();
+        if (shouldInsertSpace) EditorGUILayout.Space();
 
-        if (propOffsetEnabled.boolValue = EditorGUILayout.Toggle ("Offset Control", propOffsetEnabled.boolValue))
+        EditorGUILayout.PropertyField(propOffsetEnabled, labelOffsetControl);
+        if (propOffsetEnabled.hasMultipleDifferentValues || propOffsetEnabled.boolValue)
         {
-            propOffsetKnobIndex.intValue = EditorGUILayout.IntField ("MIDI CC #", propOffsetKnobIndex.intValue);
-            propOffsetKnobChannel.intValue = EditorGUILayout.IntPopup ("MIDI Channel", propOffsetKnobChannel.intValue, midiChannelLabels, midiChannelOptions);
-            propOffsetCurve.animationCurveValue = EditorGUILayout.CurveField ("Curve", propOffsetCurve.animationCurveValue);
+            EditorGUILayout.PropertyField(propOffsetKnobIndex, labelMidiCC);
+            EditorGUILayout.IntPopup(propOffsetKnobChannel, midiChannelLabels, midiChannelOptions, labelMidiChannel);
+            EditorGUILayout.PropertyField(propOffsetCurve, labelCurve);
         }
         
-        if (propGainEnabled.boolValue || propOffsetEnabled.boolValue)
-            EditorGUILayout.Space ();
+        if (shouldInsertSpace) EditorGUILayout.Space();
 
         // General option.
+        if (!propSensitivity.hasMultipleDifferentValues)
         {
             var value = propSensitivity.floatValue;
             if (value > 0.0f)
-                value = EditorGUILayout.Slider ("Sensitivity", (value - 0.1f) / 30, 0.0f, 1.0f);
+                value = EditorGUILayout.Slider("Sensitivity", (value - 0.1f) / 30, 0.0f, 1.0f);
             else
-                value = EditorGUILayout.Slider ("(Filter Off)", 1.0f, 0.0f, 1.0f);
+                value = EditorGUILayout.Slider("(Filter Off)", 1.0f, 0.0f, 1.0f);
             propSensitivity.floatValue = (value == 1.0f) ? 0.0f : value * 30 + 0.1f;
         }
+        else
+        {
+            EditorGUILayout.PropertyField(propSensitivity);
+        }
 
+        if (!propDecaySpeed.hasMultipleDifferentValues)
         {
             var value = propDecaySpeed.floatValue;
             if (value < 10.5f)
-                value = EditorGUILayout.Slider ("Decay Speed", (value - 0.5f) / 10, 0.0f, 1.0f);
+                value = EditorGUILayout.Slider("Decay Speed", (value - 0.5f) / 10, 0.0f, 1.0f);
             else
-                value = EditorGUILayout.Slider ("(Infinite)", 1.0f, 0.0f, 1.0f);
+                value = EditorGUILayout.Slider("(Infinite)", 1.0f, 0.0f, 1.0f);
             propDecaySpeed.floatValue = (value == 1.0f) ? 100.0f : value * 10f + 0.5f;
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(propDecaySpeed);
         }
 
         // Audio input options.
-        if (propAudioMode.intValue > 0)
+        if (propAudioMode.hasMultipleDifferentValues || propAudioMode.intValue > 0)
         {
-            if (propShowAudioOptions.boolValue = EditorGUILayout.Foldout (propShowAudioOptions.boolValue, "Audio Input Options"))
+            if (!propShowAudioOptions.hasMultipleDifferentValues)
             {
-                EditorGUILayout.Slider (propHeadroom, 0.0f, 20.0f, "Headroom [dB]");
-                EditorGUILayout.Slider (propDynamicRange, 1.0f, 60.0f, "Dynamic Range [dB]");
-                EditorGUILayout.Slider (propLowerBound, -100.0f, -10.0f, "Lower Bound [dB]");
-                EditorGUILayout.Slider (propFalldown, 0.0f, 10.0f, "Falldown [dB/Sec]");
+                propShowAudioOptions.boolValue = EditorGUILayout.Foldout(propShowAudioOptions.boolValue, "Audio Input Options");
+            }
+
+            if (propShowAudioOptions.hasMultipleDifferentValues || propShowAudioOptions.boolValue)
+            {
+                EditorGUILayout.Slider(propHeadroom, 0.0f, 20.0f, "Headroom [dB]");
+                EditorGUILayout.Slider(propDynamicRange, 1.0f, 60.0f, "Dynamic Range [dB]");
+                EditorGUILayout.Slider(propLowerBound, -100.0f, -10.0f, "Lower Bound [dB]");
+                EditorGUILayout.Slider(propFalldown, 0.0f, 10.0f, "Falldown [dB/Sec]");
             }
         }
 
         // Apply modifications.
-        serializedObject.ApplyModifiedProperties ();
+        serializedObject.ApplyModifiedProperties();
         
         // Draw the level bar on play mode.
         if (EditorApplication.isPlaying && !serializedObject.isEditingMultipleObjects)
         {
-            EditorGUILayout.Space ();
-            DrawInputLevelBars (target as Reaktor);
-            EditorUtility.SetDirty (target); // Make it dirty to update the view.
+            EditorGUILayout.Space();
+            DrawInputLevelBars(target as Reaktor);
+            EditorUtility.SetDirty(target); // Make it dirty to update the view.
         }
     }
 
-    #endregion
-    
-    #region Private functions
-
     // Make a texture which contains only one pixel.
-    Texture2D NewBarTexture (Color color)
+    Texture2D NewBarTexture(Color color)
     {
-        var texture = new Texture2D (1, 1);
-        texture.SetPixel (0, 0, color);
-        texture.Apply ();
+        var texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, color);
+        texture.Apply();
         return texture;
     }
 
     // Draw the input level bar.
-    void DrawInputLevelBars (Reaktor reaktor)
+    void DrawInputLevelBars(Reaktor reaktor)
     {
         if (barTextures == null)
         {
             // Make textures for drawing level bars.
             barTextures = new Texture2D[] {
-                NewBarTexture (new Color (55.0f / 255, 53.0f / 255, 45.0f / 255)),
-                NewBarTexture (new Color (250.0f / 255, 249.0f / 255, 248.0f / 255)),
-                NewBarTexture (new Color (110.0f / 255, 192.0f / 255, 91.0f / 255, 0.8f)),
-                NewBarTexture (new Color (226.0f / 255, 0, 7.0f / 255, 0.8f)),
-                NewBarTexture (new Color (249.0f / 255, 185.0f / 255, 22.0f / 255))
+                NewBarTexture(new Color(55.0f / 255, 53.0f / 255, 45.0f / 255)),
+                NewBarTexture(new Color(250.0f / 255, 249.0f / 255, 248.0f / 255)),
+                NewBarTexture(new Color(110.0f / 255, 192.0f / 255, 91.0f / 255, 0.8f)),
+                NewBarTexture(new Color(226.0f / 255, 0, 7.0f / 255, 0.8f)),
+                NewBarTexture(new Color(249.0f / 255, 185.0f / 255, 22.0f / 255))
             };
         }
         
         // Get a rectangle as a text field and fill it.
-        var rect = GUILayoutUtility.GetRect (18, 16, "TextField");
-        GUI.DrawTexture (rect, barTextures [0]);
+        var rect = GUILayoutUtility.GetRect(18, 16, "TextField");
+        GUI.DrawTexture(rect, barTextures[0]);
 
         // Draw the raw input bar.
         var temp = rect;
-        temp.width *= Mathf.Clamp01 ((reaktor.RawInput - reaktor.lowerBound) / (3 - reaktor.lowerBound));
-        GUI.DrawTexture (temp, barTextures [1]);
+        temp.width *= Mathf.Clamp01((reaktor.RawInput - reaktor.lowerBound) / (3 - reaktor.lowerBound));
+        GUI.DrawTexture(temp, barTextures[1]);
 
         // Draw the dynamic range.
         temp.x += rect.width * (reaktor.Peak - reaktor.lowerBound - reaktor.dynamicRange - reaktor.headroom) / (3 - reaktor.lowerBound);
         temp.width = rect.width * reaktor.dynamicRange / (3 - reaktor.lowerBound);
-        GUI.DrawTexture (temp, barTextures [2]);
+        GUI.DrawTexture(temp, barTextures[2]);
 
         // Draw the headroom.
         temp.x += temp.width;
         temp.width = rect.width * reaktor.headroom / (3 - reaktor.lowerBound);
-        GUI.DrawTexture (temp, barTextures [3]);
+        GUI.DrawTexture(temp, barTextures[3]);
 
         // Display the peak level value.
-        EditorGUI.LabelField (rect, "Peak: " + reaktor.Peak.ToString ("0.0") + " dB");
+        EditorGUI.LabelField(rect, "Peak: " + reaktor.Peak.ToString ("0.0") + " dB");
 
         // Draw the gain level.
         if (reaktor.gainEnabled)
-            DrawLevelBar ("Gain", reaktor.Gain, barTextures [0], barTextures [1]);
+            DrawLevelBar("Gain", reaktor.Gain, barTextures[0], barTextures[1]);
 
         // Draw the offset level.
         if (reaktor.offsetEnabled)
-            DrawLevelBar ("Offset", reaktor.Offset, barTextures [0], barTextures [1]);
+            DrawLevelBar("Offset", reaktor.Offset, barTextures[0], barTextures[1]);
 
         // Draw the output level.
-        DrawLevelBar ("Out", reaktor.Output, barTextures [0], barTextures [4]);
+        DrawLevelBar("Out", reaktor.Output, barTextures[0], barTextures[4]);
     }
 
-    void DrawLevelBar (string label, float value, Texture bg, Texture fg)
+    void DrawLevelBar(string label, float value, Texture bg, Texture fg)
     {
         // Get a rectangle as a text field and fill it.
-        var rect = GUILayoutUtility.GetRect (18, 16, "TextField");
-        GUI.DrawTexture (rect, bg);
+        var rect = GUILayoutUtility.GetRect(18, 16, "TextField");
+        GUI.DrawTexture(rect, bg);
         
         // Draw a level bar.
         var temp = rect;
         temp.width *= value;
-        GUI.DrawTexture (temp, fg);
+        GUI.DrawTexture(temp, fg);
         
         // Display the level value in percentage.
-        EditorGUI.LabelField (rect, label + ": " + (value * 100).ToString ("0.0") + " %");
+        EditorGUI.LabelField(rect, label + ": " + (value * 100).ToString("0.0") + " %");
     }
-
-    #endregion
 }
