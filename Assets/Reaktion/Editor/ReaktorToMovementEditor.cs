fe192d0bd81d3b2ec5e7f43e01ecfee3acb766ee
Keijiro Takahashi <keijiro@gmail.com>
2014-05-25 16:58:29 +0900

    Added ReaktorToMovement.

---
+++ Assets/Reaktion/Editor/ReaktorToMovementEditor.cs
@@ -0,0 +1,100 @@
+﻿//
+// Reaktion - An audio reactive animation toolkit for Unity.
+//
+// Copyright (C) 2013 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ReaktorToMovement)), CanEditMultipleObjects]
+public class ReaktorToMovementEditor : Editor
+{
+    SerializedProperty propEnableTranslation;
+    SerializedProperty propMinVelocity;
+    SerializedProperty propMaxVelocity;
+
+    SerializedProperty propEnableRotation;
+    SerializedProperty propRotationAxis;
+    SerializedProperty propMinAngularVelocity;
+    SerializedProperty propMaxAngularVelocity;
+
+    SerializedProperty propUseLocal;
+
+    GUIContent labelTranslation;
+    GUIContent labelRotation;
+    GUIContent labelMinVelocity;
+    GUIContent labelMaxVelocity;
+    GUIContent labelAxis;
+    GUIContent labelUseLocal;
+
+    void OnEnable()
+    {
+        propEnableTranslation  = serializedObject.FindProperty("enableTranslation");
+        propMinVelocity        = serializedObject.FindProperty("minVelocity");
+        propMaxVelocity        = serializedObject.FindProperty("maxVelocity");
+
+        propEnableRotation     = serializedObject.FindProperty("enableRotation");
+        propRotationAxis       = serializedObject.FindProperty("rotationAxis");
+        propMinAngularVelocity = serializedObject.FindProperty("minAngularVelocity");
+        propMaxAngularVelocity = serializedObject.FindProperty("maxAngularVelocity");
+
+        propUseLocal           = serializedObject.FindProperty("useLocal");
+
+        labelTranslation  = new GUIContent("Translation");
+        labelRotation     = new GUIContent("Rotation");
+        labelMinVelocity  = new GUIContent("Minimum Velocity");
+        labelMaxVelocity  = new GUIContent("Maximum Velocity");
+        labelAxis         = new GUIContent("Axis");
+        labelUseLocal     = new GUIContent("Local Coordinate");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        bool shouldSpace = 
+            propEnableTranslation.hasMultipleDifferentValues ||
+            propEnableTranslation.boolValue || propEnableRotation.boolValue;
+
+        EditorGUILayout.PropertyField(propEnableTranslation, labelTranslation);
+        if (propEnableTranslation.hasMultipleDifferentValues || propEnableTranslation.boolValue)
+        {
+            EditorGUILayout.PropertyField(propMinVelocity, labelMinVelocity);
+            EditorGUILayout.PropertyField(propMaxVelocity, labelMaxVelocity);
+        }
+
+        if (shouldSpace) EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(propEnableRotation, labelRotation);
+        if (propEnableRotation.hasMultipleDifferentValues || propEnableRotation.boolValue)
+        {
+            EditorGUILayout.PropertyField(propRotationAxis, labelAxis);
+            EditorGUILayout.PropertyField(propMinAngularVelocity, labelMinVelocity);
+            EditorGUILayout.PropertyField(propMaxAngularVelocity, labelMaxVelocity);
+        }
+
+        if (shouldSpace) EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(propUseLocal, labelUseLocal);
+
+        serializedObject.ApplyModifiedProperties ();
+    }
+}
---
+++ Assets/Reaktion/ReaktorToMovement.cs
@@ -0,0 +1,67 @@
+//
+// Reaktion - An audio reactive animation toolkit for Unity.
+//
+// Copyright (C) 2013 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+using System.Collections;
+
+public class ReaktorToMovement : MonoBehaviour
+{
+    public bool enableTranslation;
+    public Vector3 minVelocity;
+    public Vector3 maxVelocity;
+
+    public bool enableRotation;
+    public Vector3 rotationAxis;
+    public float minAngularVelocity;
+    public float maxAngularVelocity;
+
+    public bool useLocal = true;
+
+    Reaktor reaktor;
+
+    void Start()
+    {
+        reaktor = Reaktor.SearchAvailableFrom(gameObject);
+    }
+
+    void Update()
+    {
+        if (enableTranslation)
+        {
+            var delta = Vector3.Lerp(minVelocity, maxVelocity, reaktor.Output) * Time.deltaTime;
+            if (useLocal)
+                transform.localPosition += delta;
+            else
+                transform.position += delta;
+        }
+
+        if (enableRotation)
+        {
+            var omega = Mathf.Lerp(minAngularVelocity, maxAngularVelocity, reaktor.Output);
+            var delta = Quaternion.AngleAxis(omega * Time.deltaTime, rotationAxis);
+            if (useLocal)
+                transform.localRotation = delta * transform.localRotation;
+            else
+                transform.rotation = delta * transform.rotation;
+        }
+    }
+}
