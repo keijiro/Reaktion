dc2a4cec7bd2ff2edb2c43fcd9bf3471b35fbd1b
Keijiro Takahashi <keijiro@Super-MSX.local>
2014-06-02 13:06:07 +0900

    Added ReaktorToLight

---
+++ Assets/Reaktion/Editor/ReaktorToLight.cs
@@ -0,0 +1,80 @@
+﻿//
+// Reaktion - An audio reactive animation toolkit for Unity.
+//
+// Copyright (C) 2013, 2014 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ReaktorToLight)), CanEditMultipleObjects]
+public class ReaktorToLightEditor : Editor
+{
+    SerializedProperty propEnableIntensity;
+    SerializedProperty propIntensityFrom;
+    SerializedProperty propIntensityTo;
+
+    SerializedProperty propEnableColor;
+    SerializedProperty propColorFrom;
+    SerializedProperty propColorTo;
+
+    GUIContent labelIntensity;
+    GUIContent labelColor;
+    GUIContent labelFrom;
+    GUIContent labelTo;
+
+    void OnEnable()
+    {
+        propEnableIntensity = serializedObject.FindProperty("enableIntensity");
+        propIntensityFrom   = serializedObject.FindProperty("intensityFrom");
+        propIntensityTo     = serializedObject.FindProperty("intensityTo");
+
+        propEnableColor     = serializedObject.FindProperty("enableColor");
+        propColorFrom       = serializedObject.FindProperty("colorFrom");
+        propColorTo         = serializedObject.FindProperty("colorTo");
+
+        labelIntensity = new GUIContent("Intensity");
+        labelColor     = new GUIContent("Color");
+        labelFrom      = new GUIContent("From");
+        labelTo        = new GUIContent("To");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(propEnableIntensity, labelIntensity);
+        if (propEnableIntensity.hasMultipleDifferentValues || propEnableIntensity.boolValue)
+        {
+            EditorGUILayout.PropertyField(propIntensityFrom, labelFrom);
+            EditorGUILayout.PropertyField(propIntensityTo, labelTo);
+            EditorGUILayout.Space();
+        }
+
+        EditorGUILayout.PropertyField(propEnableColor, labelColor);
+        if (propEnableColor.hasMultipleDifferentValues || propEnableColor.boolValue)
+        {
+            EditorGUILayout.PropertyField(propColorFrom, labelFrom);
+            EditorGUILayout.PropertyField(propColorTo, labelTo);
+        }
+
+        serializedObject.ApplyModifiedProperties ();
+    }
+}
---
+++ Assets/Reaktion/ReaktorToLight.cs
@@ -0,0 +1,57 @@
+//
+// Reaktion - An audio reactive animation toolkit for Unity.
+//
+// Copyright (C) 2013, 2014 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+using System.Collections;
+
+public class ReaktorToLight : MonoBehaviour
+{
+    public bool enableIntensity;
+    public float intensityFrom = 0.1f;
+    public float intensityTo = 0.5f;
+
+    public bool enableColor;
+    public Color colorFrom = Color.black;
+    public Color colorTo = Color.white;
+
+    Reaktor reaktor;
+
+    void Start()
+    {
+        reaktor = Reaktor.SearchAvailableFrom(gameObject);
+        UpdateLight(0);
+    }
+
+    void Update()
+    {
+        UpdateLight(reaktor.Output);
+    }
+
+    void UpdateLight(float param)
+    {
+        if (enableIntensity)
+            light.intensity = Mathf.Lerp(intensityFrom, intensityTo, param);
+
+        if (enableColor)
+            light.color = Color.Lerp(colorFrom, colorTo, param);
+    }
+}
