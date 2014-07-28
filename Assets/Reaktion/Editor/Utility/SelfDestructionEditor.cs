6e28af2777f65f5c4bf79496bab7454c11d33371
Keijiro Takahashi <keijiro@Teppan.local>
2014-07-28 14:38:33 +0900

    Improved Editor UI.

--- Assets/Reaktion/Editor/Utility/SelfDestructionEditor.cs
+++ Assets/Reaktion/Editor/Utility/SelfDestructionEditor.cs
@@ -39,6 +39,29 @@ public class SelfDestructionEditor : Editor
     SerializedProperty propReferenceObject;
     SerializedProperty propReferenceName;
 
+    GUIContent labelConditionType;
+    GUIContent labelReferenceType;
+    GUIContent labelReferenceObject;
+    GUIContent labelReferenceName;
+
+    GUIContent[] conditionTypeLabels = {
+        new GUIContent("Distance"),
+        new GUIContent("Bounding Box"),
+        new GUIContent("Particle System Liveness")
+    };
+
+    int[] conditionTypeValues = { 0, 1, 2 };
+
+    GUIContent[] referenceTypeLabels = {
+        new GUIContent("Origin"),
+        new GUIContent("Fixed Point"),
+        new GUIContent("Initial Position"),
+        new GUIContent("Game Object"),
+        new GUIContent("Game Object Name")
+    };
+
+    int[] referenceTypeValues = { 0, 1, 2, 3, 4 };
+
     void OnEnable()
     {
         propConditionType = serializedObject.FindProperty("conditionType");
@@ -50,35 +73,52 @@ public class SelfDestructionEditor : Editor
         propReferencePoint  = serializedObject.FindProperty("referencePoint");
         propReferenceObject = serializedObject.FindProperty("referenceObject");
         propReferenceName   = serializedObject.FindProperty("referenceName");
+
+        labelConditionType   = new GUIContent("Condition");
+        labelReferenceType   = new GUIContent("Reference Point");
+        labelReferenceObject = new GUIContent("Game Object");
+        labelReferenceName   = new GUIContent("Name");
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
 
-        EditorGUILayout.PropertyField(propConditionType);
+        EditorGUILayout.IntPopup(propConditionType, conditionTypeLabels, conditionTypeValues, labelConditionType);
 
         if (propConditionType.hasMultipleDifferentValues ||
             propConditionType.enumValueIndex == (int)SelfDestruction.ConditionType.Distance)
+        {
+            EditorGUI.indentLevel++;
             EditorGUILayout.PropertyField(propDistance);
+            EditorGUI.indentLevel--;
+        }
 
         if (propConditionType.hasMultipleDifferentValues ||
             propConditionType.enumValueIndex == (int)SelfDestruction.ConditionType.Bounds)
-            EditorGUILayout.PropertyField(propBounds);
+            EditorGUILayout.PropertyField(propBounds, GUIContent.none);
 
-        EditorGUILayout.PropertyField(propReferenceType);
+        if (propConditionType.hasMultipleDifferentValues ||
+            propConditionType.enumValueIndex != (int)SelfDestruction.ConditionType.ParticleSystem)
+        {
+            EditorGUILayout.IntPopup(propReferenceType, referenceTypeLabels, referenceTypeValues, labelReferenceType);
 
-        if (propReferenceType.hasMultipleDifferentValues ||
-            propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.Point)
-            EditorGUILayout.PropertyField(propReferencePoint);
+            EditorGUI.indentLevel++;
 
-        if (propReferenceType.hasMultipleDifferentValues ||
-            propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.GameObject)
-            EditorGUILayout.PropertyField(propReferenceObject);
+            if (propReferenceType.hasMultipleDifferentValues ||
+                propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.Point)
+                EditorGUILayout.PropertyField(propReferencePoint, GUIContent.none);
 
-        if (propReferenceType.hasMultipleDifferentValues ||
-            propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.GameObjectName)
-            EditorGUILayout.PropertyField(propReferenceName);
+            if (propReferenceType.hasMultipleDifferentValues ||
+                propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.GameObject)
+                EditorGUILayout.PropertyField(propReferenceObject, labelReferenceObject);
+
+            if (propReferenceType.hasMultipleDifferentValues ||
+                propReferenceType.enumValueIndex == (int)SelfDestruction.ReferenceType.GameObjectName)
+                EditorGUILayout.PropertyField(propReferenceName, labelReferenceName);
+
+            EditorGUI.indentLevel--;
+        }
 
         serializedObject.ApplyModifiedProperties();
     }
