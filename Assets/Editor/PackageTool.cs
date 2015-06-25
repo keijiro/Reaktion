3a8766de490173adb8b01b9898c8d16813892ab6
Keijiro Takahashi <keijiro@ZX.local>
2015-06-25 22:23:39 +0900

    Added PackageTool.

---
+++ Assets/Editor/PackageTool.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageTool
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        string[] pathNames = { "Assets/Reaktion", "Assets/MidiJack", "Assets/OscJack" };
+        AssetDatabase.ExportPackage(pathNames, "Reaktion.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
